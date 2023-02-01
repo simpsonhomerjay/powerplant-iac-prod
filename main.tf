8118dbe3a864adefa39e3d656b571924b4ffed63
newhopemusician <95931293+newhopemusician@users.noreply.github.com>
2023-02-01 05:26:51 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+        region = "us-east-2"
+}
+
+module "network-module" {
+        source = "github.com/newhopemusician/terraform-powerplant-network-module"
+}
+
+module "infa-module" {
+        source = "github.com/newhopemusician/terraform-powerplant-infra-module"
+
+        subnetid = module.network-module.subnetid
+        sgid = module.network-module.sgid
+}
