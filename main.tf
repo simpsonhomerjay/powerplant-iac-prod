76694306c49a227a3fca5dd12760064fad0794d4
simpsonhomerjay <124053675+simpsonhomerjay@users.noreply.github.com>
2023-02-03 07:10:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,10 @@ provider "aws" {
 
 module "network-module" {
         source  = "tf.lnxservices.com/springfieldpower-2024/network-module/powerplant"
-        #version = "0.0.1"
 }
 
 module "infra-module" {
         source  = "tf.lnxservices.com/springfieldpower-2024/infra-module/powerplant"
-        #version = "0.0.1"
         
         subnetid = module.network-module.subnetid
         sgid = module.network-module.sgid
